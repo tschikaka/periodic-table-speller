5cf75d4d9ee5d997e77cc24fe08ac3dc61ee8b3b
tschikaka <chikaka23@gmail.com>
2023-05-01 13:54:20 +0100

    lastest implementation

--- js/app.js
+++ js/app.js
@@ -1,71 +1,70 @@
 import Speller from "./speller.js";
 
-
 if (/complete|interactive|loaded/.test(document.readyState)) {
-	ready();
+  ready();
+} else {
+  document.addEventListener("DOMContentLoaded", ready);
 }
-else {
-	document.addEventListener("DOMContentLoaded",ready);
-}
-
 
 // ****************************
 
-function ready(){
-	var enterWordEl = document.getElementById("enter-word");
-	var spellBtn = document.getElementById("spell-btn");
-	var wordSpellingEl = document.getElementById("word-spelling");
+function ready() {
+  var enterWordEl = document.getElementById("enter-word");
+  var spellBtn = document.getElementById("spell-btn");
+  var wordSpellingEl = document.getElementById("word-spelling");
 
-	enterWordEl.addEventListener("keydown",onKeydown,false);
-	spellBtn.addEventListener("click",checkWord,false);
+  enterWordEl.addEventListener("keydown", onKeydown, false);
+  spellBtn.addEventListener("click", checkWord, false);
+
+  // ********************************
+
+  function onKeydown(evt) {
+    if (evt.key == "Enter") {
+      checkWord();
+    }
+  }
+
+  function checkWord() {
+    var inputWord = enterWordEl.value.toLowerCase().trim();
+    enterWordEl.value = inputWord;
+
+    // validate the input
+    if (!/^[a-z]{3,}$/.test(inputWord)) {
+      alert("Enter a word at least 3 letters long!");
+      return;
+    }
+
+    // attempt to spell word
+    var symbols = Speller.check(inputWord);
+
+    if (!symbols) {
+      return (wordSpellingEl.innerHTML =
+        "<strong>-- couldn't spell it! --</strong>");
+    }
+    // was a valid spelling found?
+    enterWordEl.value = "";
+    spellWord(symbols);
+  }
+
+  function spellWord(symbols) {
+    wordSpellingEl.innerHTML = "";
+
+    for (let symbol of symbols) {
+      let elementEntry = Speller.lookup(symbol);
+      let elementDiv = document.createElement("div");
+      elementDiv.className = "element";
 
 
-	// ********************************
 
-	function onKeydown(evt) {
-		if (evt.key == "Enter") {
-			checkWord();
-		}
-	}
-
-	function checkWord() {
-		var inputWord = enterWordEl.value.toLowerCase().trim();
-		enterWordEl.value = inputWord;
-
-		// validate the input
-		if (!/^[a-z]{3,}$/.test(inputWord)) {
-			alert("Enter a word at least 3 letters long!");
-			return;
-		}
-
-		// attempt to spell word
-		var symbols = Speller.check(inputWord);
-
-		// was a valid spelling found?
-		if (symbols.length > 0) {
-			enterWordEl.value = "";
-			spellWord(symbols);
-		}
-		else {
-			wordSpellingEl.innerHTML = "<strong>-- couldn't spell it! --</strong>";
-		}
-	}
-
-	function spellWord(symbols) {
-		wordSpellingEl.innerHTML = "";
-
-		for (let symbol of symbols) {
-			let elementEntry = Speller.lookup(symbol);
-			let elementDiv = document.createElement("div");
-			elementDiv.className = "element";
-			elementDiv.innerHTML = `
+      
+      elementDiv.innerHTML = `
 				<div class="number">${elementEntry.number}</div>
 				<div class="symbol">${elementEntry.symbol}</div>
 				<div class="name">${elementEntry.name}</div>
 			`;
-			wordSpellingEl.appendChild(elementDiv);
-		}
-	}
+      wordSpellingEl.appendChild(elementDiv);
+    }
+  }
 }
 
 // TEST WORDS
--- js/speller.js
+++ js/speller.js
@@ -1,30 +1,54 @@
 export default {
-	check,
-	lookup,
+  check,
+  lookup,
 };
 
 var elements;
+var elementsIndex = {}
 
 await loadPeriodicTable();
-
 
 // ****************************
 
 async function loadPeriodicTable() {
-	elements = await (await fetch("periodic-table.json")).json();
+  elements = await (await fetch("periodic-table.json")).json();
+
+	elements.forEach(element => elementsIndex[element.symbol.toLowerCase()] = element);
 }
 
 function check(inputWord) {
-	// TODO: determine if `inputWord` can be spelled
-	// with periodic table symbols; return array with
-	// them if so (empty array otherwise)
+  // TODO: determine if `inputWord` can be spelled
+  // with periodic table symbols; return array with
+  // them if so (empty array otherwise)
+  //we are going to chech our entire elements list for a portion of the word that matchs the substring of the provided input
 
-	return [];
+  if (inputWord.length > 0) {
+    for (let element in elementsIndex) {
+			let symbol = element;
+
+      if (symbol.length <= inputWord.length) {
+        if (symbol === inputWord.slice(0, symbol.length)) {
+          if (inputWord.length > symbol.length) {
+            let rest = check(inputWord.slice(symbol.length));
+
+            if (rest?.length > 0) {
+              return [symbol, ...rest];
+            }
+          } else {
+            return [symbol];
+          }
+        }
+      }
+    }
+  }
 }
 
 function lookup(elementSymbol) {
-	// TODO: return the element entry based on specified
-	// symbol (case-insensitive)
-
-	return {};
+  // TODO: return the element entry based on specified
+  // symbol (case-insensitive)
+  return (
+    elements.find(
+      (element) => element.symbol.toLowerCase() === elementSymbol
+    ) ?? {}
+  );
 }
